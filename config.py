5a2c6058400eb8bc20b78060d8c56b68a821fa5b
SNEHABHIxd <93216312+SNEHABHIxd@users.noreply.github.com>
2021-11-10 09:00:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -86,7 +86,7 @@ ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/5c1bd95f066aad81df745.p
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 
-IMG_1 = getenv("IMG_1", "ttps://telegra.ph/file/5c1bd95f066aad81df745.png")
+IMG_1 = getenv("IMG_1", "https://telegra.ph/file/5c1bd95f066aad81df745.png")
 
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/bc6ecaac6eb57cb10342c.png")
 
